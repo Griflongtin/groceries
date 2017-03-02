8ec97adb3b6635b3df943b89ed9b9505954d1991
Griffin P Longtin and Jasun Feddema <griffin@>
2017-03-01 20:45:42 -0800

    will create a grocery list website.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 $(document).ready(function(event) {
 
   var items = ["#item1", "#item2", "#item3", "#item4"]
-
   $("form").submit(function(event){
     event.preventDefault();
     var updatedItems = items.map(function(item) {
@@ -9,7 +8,10 @@ $(document).ready(function(event) {
     });
     console.log(updatedItems);
     updatedItems.sort().forEach(function(item) {
-      $("#output").append(item + "," + " ");
+      $("#output1").text(updatedItems[0]);
+      $("#output2").text(updatedItems[1]);
+      $("#output3").text(updatedItems[2]);
+      $("#output4").text(updatedItems[3]);
       $("#list").show();
       $("#form").hide();
     });
